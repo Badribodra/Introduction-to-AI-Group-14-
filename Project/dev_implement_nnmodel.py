08dba901d5e6dd1c8aa5a586d5ef6a23d5896975
phuocdoanuk <phuoc.doan@city.ac.uk>
2024-11-08 00:19:14 +0000

    Config datapaths

--- Project/dev_implement_nnmodel.py
+++ Project/dev_implement_nnmodel.py
@@ -16,12 +16,52 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 
-# Dataset processing
+# Dataset Preparation
 # TODO: should be dedicated classes/functions
 
 # manual configuration
+# chest-xray-small:
+# link: https://www.kaggle.com/datasets/pcbreviglieri/pneumonia-xray-images/data
+# chest-xray-full:
+# link: https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia/data
 datasets  = ['chest-xray-small', 'chest-xray-full']
 select_dataset = 0
-dataset_original_path = f"./datasets/{datasets[0]}"
+dataset_original_path = f"./datasets/{datasets[select_dataset]}"
+
+print(dataset_original_path)
+
+# Setup labels, data paths, etc...
+num_classes = 2
+labels = ['Normal', 'Opacity']
+
+data_paths = {'normal': [
+                    f"{dataset_original_path}/train/normal",
+                    f"{dataset_original_path}/val/normal",
+                    f"{dataset_original_path}/test/normal"
+                        ],
+             'opacity': [
+                    f"{dataset_original_path}/train/opacity",
+                    f"{dataset_original_path}/val/opacity",
+                    f"{dataset_original_path}/test/opacity"
+             ]}
+
+for k,v in data_paths.items():
+    print(k, v)
+data2_paths = {}
+for label in labels:
+    data2_paths[label] = [
+        f"{dataset_original_path}/train/{label}",
+        f"{dataset_original_path}/val/{label}",
+        f"{dataset_original_path}/test/{label}"
+    ]
+
+for k,v in data2_paths.items():
+    print(k, v)
+
+
+
+
+
+
 
 
