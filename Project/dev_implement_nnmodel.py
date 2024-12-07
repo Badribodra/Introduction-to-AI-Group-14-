a1e441d9b11b6552b00579a07e0354110fc153c8
phuocdoanuk <phuoc.doan@city.ac.uk>
2024-12-07 19:06:35 +0000

    Update 1st version of cnn model for sportsballs dataset

--- Project/dev_implement_nnmodel.py
+++ Project/dev_implement_nnmodel.py
@@ -5,6 +5,11 @@
 ## start
 
 ## end --
+## TIMESTAMP @ 2024-12-07T19:01:46
+## author: phuocddat
+## start
+
+## end --
 # Import necessary libs
 import os
 import pandas as pd
@@ -22,14 +27,22 @@ from dev_configuration import learning_rate, model_loss_opt
 isDebug = True
 isVerbose = True
 randomState_value = 2024
+# hard code for now.
+dataset_name = 'sport-balls'
 
-
-loaded_datasets = dev_dataprocesses.load_dataset(preconfigured_dataset="Chest")
+loaded_datasets = dev_dataprocesses.load_dataset(preconfigured_dataset=dataset_name)
 
 train_data, val_data, test_data = dev_dataprocesses.data_generators(datasource=loaded_datasets,
                                                                     data_generator='keras')
 
 # Model building
+# experimental model architecture number 1
+"""
+ModelArchName: CNN_exp_no1
+Note: number of classes is harcoded. Should be variable. 
+"""
+# training from scratch....
+
 model_exp_1 = keras.models.Sequential([
     keras.layers.Conv2D(filters=128, kernel_size=(8, 8), strides=(3, 3), activation='relu', input_shape=(224, 224, 3)),
     keras.layers.BatchNormalization(),
@@ -50,21 +63,34 @@ model_exp_1 = keras.models.Sequential([
     keras.layers.Flatten(),
     keras.layers.Dense(1024, activation='relu'),
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(2, activation='softmax')
+    keras.layers.Dense(15, activation='softmax')
 ])
 
+## TIMESTAMP @ 2024-12-07T19:02:58
+## author: phuocddat
+## start
+"""
+model compile 
+arch: CNN_exp_no1
+loss: model_loss_opt (from dev_configuration)
+optimizer: SGD with lr from dev_configuration
+metrics: ['accuracy']
+"""
+## end --
 model_exp_1.compile(
     loss=model_loss_opt,
     optimizer=tf.optimizers.SGD(learning_rate=learning_rate),
     metrics=['accuracy']
 )
 
+# get summary of model.
 model_exp_1.summary()
 
 
 # Exp model training
+# hardcode everything.
 training_exper = model_exp_1.fit(train_data,
-                                 epochs=5,
+                                 epochs=10,
                                  validation_data=val_data,
                                  verbose=1)
 # Evaluate model
